25ab9c6128eaa64d5a7e8b9c108297bf3aa84b64
Alex <suarez9093@hotmail.com>
2019-11-25 22:34:22 -0700

    check letter function working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,5 @@
 
-var wordBank = ["Apple","Pear","Blueberry"];
+var wordBank = ["apple", "pear", "blueberry"];
 var selectedWord = "";
 var lettersInSelectedWord = [];
 var numBlanks = 0;
@@ -11,33 +11,38 @@ var wins = 0;
 var losses = 0;
 var guessesRemaining = 9;
 
-function startGame(){
-    selectedWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+function startGame() {
+    selectedWord = wordBank[Math.floor(Math.random() * wordBank.length)];
     console.log("selectedWord: ", selectedWord);
     lettersInSelectedWord = selectedWord.split("");
-    console.log("lettersInSelectedWord: ",lettersInSelectedWord);
+    console.log("lettersInSelectedWord: ", lettersInSelectedWord);
     numBlanks = lettersInSelectedWord.length;
-    console.log("numBlanks",numBlanks);
+    console.log("numBlanks", numBlanks);
 
-    for(let i = 0; i < numBlanks; i ++){
+    for (let i = 0; i < numBlanks; i++) {
         answerDisplay.push("_");
-        console.log("answerDisplay: ",answerDisplay)
+        console.log("answerDisplay: ", answerDisplay)
     }
 
     document.getElementById("selected_word").innerHTML = answerDisplay.join(" ")
 };
 
-function checkLetters(letter){
-    console.log("letter: ",letter)
+function checkLetters(letter) {
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        for (var i = 0; i < numBlanks; i++) {
+            if (selectedWord[i] === letter) {  
+                answerDisplay[i] = letter;
+            }
+        }
+    }
 };
-
 
 startGame();
 
-document.onkeyup = function(event){
-    console.log("event: ",event)
+document.onkeyup = function (event) {
+    // console.log("event: ",event)
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log("letterGuessed: ",letterGuessed)
+    // console.log("letterGuessed: ",letterGuessed)
 
     checkLetters(letterGuessed)
 };

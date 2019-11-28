2b1dd1159f62c333523fa336fd2ba35cbb80da45
Alex <suarez9093@hotmail.com>
2019-11-27 23:28:00 -0700

    game working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,5 @@
 
-var wordBank = ["apple", "pear", "blueberry"];
+var wordBank = ["goku", "vegeta", "trunks"];
 var selectedWord = "";
 var lettersInSelectedWord = [];
 var numBlanks = 0;
@@ -19,6 +19,9 @@ function startGame() {
     numBlanks = lettersInSelectedWord.length;
     console.log("numBlanks", numBlanks);
 
+    guessesRemaining = 9;
+		wrongLetters = [];
+		answerDisplay = [];
     for (let i = 0; i < numBlanks; i++) {
         answerDisplay.push("_");
         console.log("answerDisplay: ", answerDisplay)
@@ -48,12 +51,30 @@ function checkLetters(letter){
     console.log("answerDisplay: ",answerDisplay);
     console.log("wrongLetters: ",wrongLetters);
     console.log("guessesRemaining: ",guessesRemaining);
+    
+}
+
+function endRound(){
     document.getElementById("selected_word").innerHTML = answerDisplay.join(" ")
-    document.getElementById("wrongLetters").innerHTML = wrongLetters
+    document.getElementById("wrongLetters").innerHTML = "Letters Guessed:" + " " + wrongLetters.join(" ")
     document.getElementById("guessesRemaining").innerHTML = "Guesses Remaining: " + guessesRemaining
+
+    if(lettersInSelectedWord.toString() == answerDisplay.toString()){
+        wins++
+        document.getElementById("wins").innerHTML = "Wins:" + " " + wins;
+        document.getElementById("wrongLetters").innerHTML = "Letters Guessed:"
+        startGame();
+    } else if (guessesRemaining == 0) {
+        losses ++;
+        document.getElementById("losses").innerHTML = "Losses:" + " " + losses;
+        document.getElementById("wrongLetters").innerHTML = "Letters Guessed:"
+        startGame();
+    }
 }
 
 
+// Main Process
+// ====================================================================
 
 startGame();
 
@@ -63,6 +84,37 @@ document.onkeyup = function (event) {
     // console.log("letterGuessed: ",letterGuessed)
 
     checkLetters(letterGuessed)
+    endRound()
+
+    if( guessesRemaining == 8){
+        document.getElementById("goku").classList.add("dead")
+        document.getElementById("audio").play();
+       
+    }
+    if( guessesRemaining == 7){
+        document.getElementById("vegeta").classList.add("dead")
+        document.getElementById("audio").play();
+    }
+    if( guessesRemaining == 6){
+        console.log("6")
+    }
+    if( guessesRemaining == 5){
+        console.log("5")
+    }
+    if( guessesRemaining == 4){
+        console.log("4")
+    }
+    if( guessesRemaining == 3){
+        console.log("3")
+    }
+    if( guessesRemaining == 2){
+        console.log("2")
+    }
+    if( guessesRemaining == 1){
+        console.log("1")
+    }
 };
+
+
 
 console.log(guessesRemaining)
